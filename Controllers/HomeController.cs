c6e648300733d146073a3489075a6aa6373668bf
DanWartsbaugh <d.wartsbaugh@gmail.com>
2023-06-12 08:00:06 -0500

    first commit - initital layouts

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SaintBarnabasHouse.Models;
+
+namespace SaintBarnabasHouse.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    [HttpGet("blog")]
+    public IActionResult Blog()
+    {
+        return View("Blog");
+    }
+
+        [HttpGet("shop")]
+    public IActionResult Shop()
+    {
+        return View("Shop");
+    }
+
+        [HttpGet("event")]
+    public IActionResult Event()
+    {
+        return View("Event");
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SaintBarnabasHouse.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
